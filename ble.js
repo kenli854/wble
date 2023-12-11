3ef1a4f6e3f05f0aa8df31019c885398045d417f
Ken Li <kenli854@gmail.com>
2023-12-11 15:18:18 +0800

    Update ble.js

--- ble.js
+++ ble.js
@@ -1,30 +1,44 @@
-document.getElementById('scanButton').addEventListener('click', async () => {
-    try {
-        const devices = await navigator.bluetooth.requestDevice({
-            acceptAllDevices: true,
-            optionalServices: ['heart_rate'] // Adjust the service UUID as needed
-        });
+function onWatchAdvertisementsButtonClick() {
+  log('Requesting any Bluetooth device...');
+  navigator.bluetooth.requestDevice({
+// filters: [...] <- Prefer filters to save energy & show relevant devices.
+    acceptAllDevices: true
+  })
+  .then(device => {
+    log('> Requested ' + device.name);
 
-        const deviceList = document.getElementById('deviceList');
-        deviceList.innerHTML = ''; // Clear previous entries
+    device.addEventListener('advertisementreceived', (event) => {
+      log('Advertisement received.');
+      log('  Device Name: ' + event.device.name);
+      log('  Device ID: ' + event.device.id);
+      log('  RSSI: ' + event.rssi);
+      log('  TX Power: ' + event.txPower);
+      log('  UUIDs: ' + event.uuids);
+      event.manufacturerData.forEach((valueDataView, key) => {
+        logDataView('Manufacturer', key, valueDataView);
+      });
+      event.serviceData.forEach((valueDataView, key) => {
+        logDataView('Service', key, valueDataView);
+      });
+    });
 
-        devices.forEach(device => {
-            if (device.name && device.name.toUpperCase().startsWith('ACR')) {
-                const listItem = document.createElement('li');
-                listItem.textContent = device.name;
-                listItem.addEventListener('click', async () => {
-                    try {
-                        const server = await device.gatt.connect();
-                        // Now you can interact with the device's services and characteristics
-                        console.log('Connected to:', device.name);
-                    } catch (error) {
-                        console.error('Error connecting to device:', error);
-                    }
-                });
-                deviceList.appendChild(listItem);
-            }
-        });
-    } catch (error) {
-        console.error('Error scanning for devices:', error);
-    }
-});
+    log('Watching advertisements from "' + device.name + '"...');
+    return device.watchAdvertisements();  
+  })
+  .catch(error => {
+    log('Argh! ' + error);
+  });
+}
+
+/* Utils */
+
+const logDataView = (labelOfDataSource, key, valueDataView) => {
+  const hexString = [...new Uint8Array(valueDataView.buffer)].map(b => {
+    return b.toString(16).padStart(2, '0');
+  }).join(' ');
+  const textDecoder = new TextDecoder('ascii');
+  const asciiString = textDecoder.decode(valueDataView.buffer);
+  log(`  ${labelOfDataSource} Data: ` + key +
+      '\n    (Hex) ' + hexString +
+      '\n    (ASCII) ' + asciiString);
+};
